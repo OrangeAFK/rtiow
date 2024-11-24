fc9030b9878870cad78f82261219c94e4f0acde1
OrangeAFK <onepluslion@gmail.com>
2024-11-24 00:22:25 -0600

    added write_color in color.h and used vec3.h and color.h in main

---
+++ color.h
@@ -0,0 +1,22 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include <iostream>
+
+void write_color(std::ostream& out, const vec3& pixel_color)
+{
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // translate [0, 1] components to [0, 255]
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    // write out pixel color components
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif
