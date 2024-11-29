e0b2020593522ebe276af38bb0ca153ca3430059
OrangeAFK <onepluslion@gmail.com>
2024-11-29 05:16:42 -0600

    lambertian diffuse and gamma correction

--- color.h
+++ color.h
@@ -4,11 +4,24 @@
 #include "interval.h"
 #include "vec3.h"
 
+inline double linear_to_gamma(double linear_component)
+{
+    if (linear_component > 0)
+        return std::sqrt(linear_component);
+
+    return 0;
+}
+
 void write_color(std::ostream& out, const vec3& pixel_color)
 {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // Apply a linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // translate [0, 1] components to [0, 255]
     static const interval intensity(0, 0.999);
